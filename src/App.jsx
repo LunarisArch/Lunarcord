cc297b8b3b917e700cac8491d0583c5a7f046775
Ryan <simmonsryan779@gmail.com>
2026-05-22 18:03:13 -0700

    landing page update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import VerifyEmail from './Pages/VerifyEmail.jsx';
 import Auth from './Pages/Auth'
 import Lost from './Pages/404.jsx';
 import Landing from './Pages/Landing.jsx';
+import Dashboard from './Pages/Dashboard.jsx';
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
-      <Route path='/authentication' element={<Auth />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/auth' element={<Auth />} />
       <Route path="*" element={<Lost />} />
       <Route path='/verify-email' element={<VerifyEmail />} />
     </Routes>
